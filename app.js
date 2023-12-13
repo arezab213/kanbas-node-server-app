1aea9db5e803e14a4c1259700123f637a0be08c1
Amir Bagheri <63615066+arezab213@users.noreply.github.com>
2023-12-13 02:16:22 -0500

    finished 4.3 modules section

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import Hello from "./hello.js"
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./Modules/routes.js";
 import cors from "cors";
 
 const app = express()
@@ -10,4 +11,5 @@ app.use(express.json());
 Lab5(app);
 Hello(app);
 CourseRoutes(app);
+ModuleRoutes(app);
 app.listen(4000)
