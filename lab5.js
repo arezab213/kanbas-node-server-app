cfd5397cafdd9566ca62e0dd4cac00f9fd7cdce5
Amir Bagheri <63615066+arezab213@users.noreply.github.com>
2023-12-13 00:15:29 -0500

    completed lab 3.5 + extra credit

--- lab5.js
+++ lab5.js
@@ -111,6 +111,38 @@ const Lab5 = (app) => {
     todos.splice(todos.indexOf(todo), 1);
     res.json(todos);
   });
+
+  app.post("/a5/todos", (req, res) => {
+    const newTodo = {
+      ...req.body,
+      id: new Date().getTime(),
+    };
+    todos.push(newTodo);
+    res.json(newTodo);
+  });
+  app.delete("/a5/todos/:id", (req, res) => {
+    const {id} = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    if (!todo) {
+      res.status(404).json({message: `Unable to delete Todo with ID ${id}`});
+      return;
+    }
+    todos.splice(todos.indexOf(todo), 1);
+    res.sendStatus(200);
+  });
+  app.put("/a5/todos/:id", (req, res) => {
+    const {id} = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    if (!todo) {
+      res.status(404).json({message: `Unable to update Todo with ID ${id}`});
+      return;
+    }
+    todo.title = req.body.title;
+    todo.description = req.body.description;
+    todo.due = req.body.due;
+    todo.completed = req.body.completed;
+    res.sendStatus(200);
+  });
 };
 
 export default Lab5;
