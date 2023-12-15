e1fb378aa71d436aa900031d748a32b40404c649
Amir Bagheri <63615066+arezab213@users.noreply.github.com>
2023-12-14 20:40:18 -0500

    completed up to 3.5.1 lab

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express()
 app.use(cors());
 app.use(express.json());
 Lab5(app);
 Hello(app);
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
